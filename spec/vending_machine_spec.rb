952f1f92f039ac2facf0ee4f733097aa7883de63
Ash Moran <ash.moran@patchspace.co.uk>
2012-07-12 22:15:18 +0100

    Dispense an item

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -1,15 +1,24 @@
 require 'spec_helper'
 
 class VendingMachine
+  def initialize(dispenser)
+    @dispenser = dispenser 
+  end
+
   def vend
-    
+    @dispenser.release_item
   end
 end
 
 describe VendingMachine do
-  subject(:vending_machine) { VendingMachine.new }
+  let(:dispenser) { double("Dispenser", release_item: nil) }
+  subject(:vending_machine) { VendingMachine.new(dispenser) }
 
-  it "vends" do
-    vending_machine.vend
+  context "a single item, no charge, stock unlimited" do
+    it "vends" do
+      dispenser.should_receive(:release_item)
+      vending_machine.vend
+    end
   end
+
 end
