6b92e3b5d474ca493e850118b517c33831fc11f8
Ash Moran <ash.moran@patchspace.co.uk>
2012-08-27 00:01:02 +0100

    Equally naive three-book discount

--- lib/potter/special_offers.rb
+++ lib/potter/special_offers.rb
@@ -1,3 +1,5 @@
+require 'bigdecimal'
+
 class SpecialOffers
   def initialize
     @items = []
@@ -8,8 +10,10 @@ class SpecialOffers
   end
 
   def apply_discounts(receipt)
-    if @items.include?("A") && @items.include?("B")
-      receipt.record_item("Two book set", -0.8)
+    if @items.include?("A") && @items.include?("B") && @items.include?("C")
+      receipt.record_item("Three book set", -(8 * 3 * BigDecimal.new("0.1")))
+    elsif @items.include?("A") && @items.include?("B")
+      receipt.record_item("Two book set", -(8 * 2 * BigDecimal.new("0.05")))
     end
   end
 end
--- spec/potter/special_offers_spec.rb
+++ spec/potter/special_offers_spec.rb
@@ -32,5 +32,27 @@ describe SpecialOffers do
         special_offers.apply_discounts(receipt)
       end
     end
+
+    context "two different books among three" do
+      it "applies a 5% discount" do
+        special_offers.remember_item("A")
+        special_offers.remember_item("B")
+        special_offers.remember_item("B")
+
+        receipt.should_receive(:record_item).with("Two book set", -0.8)
+        special_offers.apply_discounts(receipt)
+      end
+    end
+
+    context "three different books" do
+      it "applies a 10% discount" do
+        special_offers.remember_item("A")
+        special_offers.remember_item("B")
+        special_offers.remember_item("C")
+
+        receipt.should_receive(:record_item).with("Three book set", -2.4)
+        special_offers.apply_discounts(receipt)
+      end
+    end
   end
 end
