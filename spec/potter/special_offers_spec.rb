508b3944d057ef018aef3959ce12bf34bbba56d5
Ash Moran <ash.moran@patchspace.co.uk>
2012-08-26 23:01:07 +0100

    No discounts from SpecialOffers does not record any line items

--- spec/potter/special_offers_spec.rb
+++ spec/potter/special_offers_spec.rb
@@ -1,21 +1,10 @@
 require 'spec_helper'
 
 require 'potter/special_offers'
-
-class Receipt; end
+require 'potter/receipt'
 
 describe SpecialOffers do
   subject(:special_offers) { SpecialOffers.new }
-
-  describe "#remember_item" do
-    it "doesn't throw an error" do
-      special_offers.remember_item("A")
-    end
-
-    it "does something useful" do
-      pending
-    end
-  end
 
   describe "#apply_discounts" do
     let(:receipt) { mock(Receipt) }
@@ -24,8 +13,14 @@ describe SpecialOffers do
       special_offers.apply_discounts(receipt)
     end
 
-    it "does something useful" do
-      pending
+    context "no offers" do
+      it "does nothing to the receipt" do
+        special_offers.remember_item("A")
+        special_offers.remember_item("A")
+
+        receipt.should_not_receive(:record_item)
+        special_offers.apply_discounts(receipt)
+      end
     end
   end
 end
