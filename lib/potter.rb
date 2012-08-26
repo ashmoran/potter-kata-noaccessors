198a19ff4242983182fcf7efc9bfce7d87b20a9e
Ash Moran <ash.moran@patchspace.co.uk>
2012-08-26 22:43:31 +0100

    Finish extracting all the code

--- lib/potter.rb
+++ lib/potter.rb
@@ -1,19 +1,4 @@
 require 'potter/special_offers'
 
-class PotterShop
-  def find_a_checkout_assitant_to_sell(options)
-    customer        = options[:to_customer]
-    receipt         = Receipt.new(customer)
-    special_offers  = SpecialOffers.new
-    checkout        = Checkout.new(receipt, special_offers)
-    CheckoutAssistant.new(checkout)
-  end
-end
-
-class Customer
-  def buy_books(barcodes, options)
-    options[:from_shop].
-      find_a_checkout_assitant_to_sell(to_customer: self).
-      sell_me_these_books(barcodes)
-  end
-end
+require 'potter/customer'
+require 'potter/potter_shop'
--- spec/potter_spec.rb
+++ spec/potter_spec.rb
@@ -2,39 +2,18 @@ require 'spec_helper'
 
 require 'potter'
 
-describe PotterShop do
+describe "Potter" do
   let(:customer) { Customer.new }
   subject(:shop) { PotterShop.new }
-
-  describe "#find_a_checkout_assitant_to_sell" do
-    it "builds a CheckoutAssistant" do
-      shop.
-        find_a_checkout_assitant_to_sell(to_customer: customer).
-        should be_a(CheckoutAssistant)
-    end
-  end
 
   it "tells the customer what they owe" do
     # TODO: this should not me a message expectation
     customer.should_receive(:your_price_to_pay_is).with(16)
     customer.buy_books(%w[ A A ], from_shop: shop)
   end
-end
 
-describe Customer do
-  let(:checkout_assistant) { mock(CheckoutAssistant, sell_me_these_books: nil) }
-  let(:shop) { mock(PotterShop, find_a_checkout_assitant_to_sell: checkout_assistant) }
-  subject(:customer) { Customer.new }
-
-  describe "#buy_books" do
-    it "finds a CheckoutAssistant" do
-      shop.should_receive(:find_a_checkout_assitant_to_sell).with(to_customer: customer)
-      customer.buy_books(%w[ A A ], from_shop: shop)
-    end
-
-    it "asks to buy the books" do
-      checkout_assistant.should_receive(:sell_me_these_books).with(%w[ A A ])
-      customer.buy_books(%w[ A A ], from_shop: shop)
-    end
+  it "answers the kata example", pending: true do
+    customer.should_receive(:your_price_to_pay_is).with(52.20)
+    customer.buy_books(%w[ A A B B C C D E], from_shop: shop)
   end
 end
