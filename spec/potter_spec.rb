8d97ec7d505d1c0aa57180a5182be0fec86a54a0
Ash Moran <ash.moran@patchspace.co.uk>
2012-09-09 11:28:20 +0100

    Clean up the main spec (with the weird edge case) - still failing

--- spec/potter_spec.rb
+++ spec/potter_spec.rb
@@ -7,19 +7,20 @@ describe "Potter" do
   subject(:shop) { PotterShop.new }
 
   it "tells the customer what they owe" do
-    # TODO: this should not me a message expectation
+    # TODO: this should not be a message expectation
     customer.should_receive(:your_price_to_pay_is).with(16)
     customer.buy_books(%w[ A A ], from_shop: shop)
   end
 
   it "applies discounts" do
-    # TODO: this should not me a message expectation
-    customer.should_receive(:your_price_to_pay_is).with(15.2)
+    # TODO: this should not be a message expectation
+    customer.should_receive(:your_price_to_pay_is).with(BigDecimal.new("15.2"))
     customer.buy_books(%w[ A B ], from_shop: shop)
   end
 
-  it "answers the kata example", pending: true do
-    customer.should_receive(:your_price_to_pay_is).with(52.20)
-    customer.buy_books(%w[ A A B B C C D E], from_shop: shop)
+  it "answers the kata example" do
+    # TODO: this should not be a message expectation
+    customer.should_receive(:your_price_to_pay_is).with(BigDecimal.new("52.20"))
+    customer.buy_books(%w[ A A B B C C D E ], from_shop: shop)
   end
 end
